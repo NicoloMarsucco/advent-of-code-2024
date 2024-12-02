5fed95cafe7c44df4920e0edb9867d3a9d9bb11a
NicoloMarsucco <nicolomarsucco@gmail.com>
2024-12-02 14:30:04 +0000

    Refactor code
    The solution of firts puzzle is now wrapped inside a method. This allows to use it for the second puzzle as well.

--- DayTwo.java
+++ DayTwo.java
@@ -7,13 +7,19 @@ public class DayTwo {
     private int minAbsoluteChangeInLevel = 1;
     private int maxAbsoluteChangeInLevel = 3;
     private int safeReportsCount = 0;
+    private int safeReportsCountAfterDampener = 0;
     private String filename;
 
     public DayTwo(String filename) {
         this.filename = filename;
+        solvePuzzles();
     }
 
     public int getFirstExerciseSolution() {
+        return this.safeReportsCount;
+    }
+
+    private int solvePuzzles() {
         
         try (Scanner scanner = new Scanner(Paths.get(this.filename))) {
 
@@ -24,45 +30,19 @@ public class DayTwo {
 
                 //Convert to int array
                 int[] partsAsInt = Arrays.asList(partsAsStrings).stream().mapToInt(Integer::parseInt).toArray();
-                boolean isThisValidRow = true;
+               
+                boolean isRowSafe = checkIfSafe(partsAsInt);
+
+                if (isRowSafe) {
+                    this.safeReportsCount++;
+                }
+
                 
 
-                // Look at first difference
-                int firstDifference = partsAsInt[1] - partsAsInt[0];
-                boolean isPositive = checkIfPositive(firstDifference);
-                int absoluteFirstDifference = Math.abs(firstDifference);
-
-                //Check if firts difference is withing range
-                if (absoluteFirstDifference < this.minAbsoluteChangeInLevel || absoluteFirstDifference > this.maxAbsoluteChangeInLevel) {
-                    isThisValidRow = false;
-                }
-
-                if (isThisValidRow) {
-                    for (int i = 2; i < partsAsInt.length; i++ ) {
-                        int difference = partsAsInt[i] - partsAsInt[i-1];
-
-                        //Check max diff criterion
-                        if (Math.abs(difference) > this.maxAbsoluteChangeInLevel || Math.abs(difference) < this.minAbsoluteChangeInLevel) {
-                            isThisValidRow = false;
-                            break;
-                        }
-
-                        // Check sign criterion
-                        if (checkIfPositive(difference) != isPositive) {
-                            isThisValidRow = false;
-                            break;
-                        }
-
-                    }
-
-                    // Add to count
-                    if (isThisValidRow) {
-                        this.safeReportsCount++;
-                    }
-                }
 
                 
             }
+
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
@@ -72,6 +52,40 @@ public class DayTwo {
 
     private boolean checkIfPositive(int difference) {
         return difference > 0;
+    }
+
+    private boolean checkIfSafe(int[] arrayOfNumbers) {
+        boolean isThisValidRow = true;
+        
+        //Look at first difference
+        int firstDifference = arrayOfNumbers[1] - arrayOfNumbers[0];
+        boolean isPositive = checkIfPositive(firstDifference);
+        int absoluteFirstDifference = Math.abs(firstDifference);
+
+        //Check if firts difference is withing range
+        if (absoluteFirstDifference < this.minAbsoluteChangeInLevel || absoluteFirstDifference > this.maxAbsoluteChangeInLevel) {
+            isThisValidRow = false;
+        }
+
+        if (isThisValidRow) {
+            for (int i = 2; i < arrayOfNumbers.length; i++ ) {
+                int difference = arrayOfNumbers[i] - arrayOfNumbers[i-1];
+
+                //Check max diff criterion
+                if (Math.abs(difference) > this.maxAbsoluteChangeInLevel || Math.abs(difference) < this.minAbsoluteChangeInLevel) {
+                    isThisValidRow = false;
+                    break;
+                }
+
+                // Check sign criterion
+                if (checkIfPositive(difference) != isPositive) {
+                    isThisValidRow = false;
+                    break;
+                }
+
+            }
+        }
+        return isThisValidRow;
     }
 
     
