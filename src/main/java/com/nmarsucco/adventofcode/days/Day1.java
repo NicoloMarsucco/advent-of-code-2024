71c1def65348f052149395d7d80429e8b9c88dd6
NicoloMarsucco <nicolomarsucco@gmail.com>
2025-04-21 12:11:14 +0200

    Remove unused Day1 imports

--- src/main/java/com/nmarsucco/adventofcode/days/Day1.java
+++ src/main/java/com/nmarsucco/adventofcode/days/Day1.java
@@ -3,12 +3,10 @@ package src.main.java.com.nmarsucco.adventofcode.days;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import src.main.java.com.nmarsucco.adventofcode.Day;
 
 public class Day1 extends Day {
