cc7c20dcc26946f885b8c67f01b7cf67f35f20bb
NicoloMarsucco <nicolomarsucco@gmail.com>
2025-04-21 12:02:04 +0200

    Add Main

---
+++ src/main/java/com/nmarsucco/adventofcode/Main.java
@@ -0,0 +1,25 @@
+package src.main.java.com.nmarsucco.adventofcode;
+
+import src.main.java.com.nmarsucco.adventofcode.days.Day1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Day[] solvedDays = { new Day1() };
+        for (Day day : solvedDays) {
+            System.out.println("Day " + day.getDayNumber() + ":");
+
+            long startTime = System.nanoTime();
+            Object part1 = day.solvePart1();
+            long part1Time = (System.nanoTime() - startTime) / 1_000_000; // Converto to milliseconds
+
+            startTime = System.nanoTime();
+            Object part2 = day.solvePart2();
+            long part2Time = (System.nanoTime() - startTime) / 1_000_000;
+
+            System.out.println("Solution of part 1: " + part1.toString() + " (" + part1Time + "ms)");
+            System.out.println("Solution of part 2: " + part2.toString() + " (" + part2Time + "ms)");
+        }
+
+    }
+}
